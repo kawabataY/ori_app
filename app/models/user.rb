08fcbd415eb04aa9f795ff3ef22bdebf289aae81
kawabataY <yukikawabata07@gmail.com>
2023-06-29 18:00:03 +0900

    新規登録バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,23 @@ class User < ApplicationRecord
 
   before_create :generate_app_id
 
+  #バリデーション
+  validates :name, presence: true
+  validates :app_id, presence: true
+
+  validate :password_complexity
+
   private
-  def generate_app_id #ユーザーが互いに見つけるためにランダムなユーザーIDを付与
+
+  def generate_app_id  #ユーザーが互いに見つけるためにランダムなユーザーIDを付与
     self.app_id = SecureRandom.hex(5)
+  end
+
+  def password_complexity
+    return if password.blank?
+
+    return if password.match?(/\A(?=.*[a-zA-Z])(?=.*\d)/)
+
+    errors.add(:password, 'は、半角英数字混合で入力してください')
   end
 end
