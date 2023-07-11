f2891646f3a9f631e2287f12dec9687d894dc135
kawabataY <yukikawabata07@gmail.com>
2023-07-11 22:08:52 +0900

    タスク削除機能

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,8 +3,8 @@ class Task < ApplicationRecord
   belongs_to :user
   belongs_to :room
   belongs_to :frequency
-  has_many :mytasks
-  has_many :comments
+  has_many :mytasks, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :frequency_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   get 'join_action', to: 'rooms#join_action', as: 'join_action'
 
   resources :rooms, only: [:index, :new, :create] do
-    resources :tasks, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
-      resources :mytasks 
+    resources :tasks do
+      resources :mytasks, only: [:create, :destroy] 
       resources :comments, only: [:edit, :update, :create]
     end
   end
