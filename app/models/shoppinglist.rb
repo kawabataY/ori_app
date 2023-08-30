4eeca2e7d8bf52d1b1e2b3f3b845ac18e87e509d
kawabataY <yukikawabata07@gmail.com>
2023-08-30 15:46:14 +0900

    買い物メモ機能バリデーション

--- app/models/shoppinglist.rb
+++ app/models/shoppinglist.rb
@@ -2,4 +2,10 @@ class Shoppinglist < ApplicationRecord
   belongs_to :room
   belongs_to :user
   belongs_to :task, optional: true
+
+  with_options presence: true do
+    validates :content, format: { without: /[!@#\$%^&*,.?":{}|<>]/, message: "cannot contain special characters" }
+    validates :room
+    validates :user 
+  end
 end
